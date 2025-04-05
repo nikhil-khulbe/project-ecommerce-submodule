99f7379a77cbe8a7252fdf98ea0f7c0a5bbf56ec
MAYANK\r_khu <enerkkhu@gmail.com>
2025-04-05 16:05:58 +0530

    Add React Native project files

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,16 @@ import {NavigationContainer} from '@react-navigation/native';
 import {Text, View} from 'react-native';
 import {MyStack} from './Navigator/StackNavigator';
 import {ThemeContextProvider} from './Context/ThemeContext';
-import { LangaugeContextProvider } from './Context/LanguageContext';
+import {LangaugeContextProvider} from './Context/LanguageContext';
 
 export default function App() {
   return (
     <LangaugeContextProvider>
-    <ThemeContextProvider>
-      <NavigationContainer>
-        <MyStack />
-      </NavigationContainer>
-    </ThemeContextProvider></LangaugeContextProvider>
+      <ThemeContextProvider>
+        <NavigationContainer>
+          <MyStack />
+        </NavigationContainer>
+      </ThemeContextProvider>
+    </LangaugeContextProvider>
   );
 }
